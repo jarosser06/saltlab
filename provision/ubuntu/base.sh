082f1b8587c83dfe4b52addcfaac60e8e902297f
Jim Rosser <jarosser06@gmail.com>
2015-02-16 09:56:33 -0600

    ... firewall

--- provision/ubuntu/base.sh
+++ provision/ubuntu/base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
-wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
+wget -q -O- "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
 apt-get update -y
